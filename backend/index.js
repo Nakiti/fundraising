616c2dc0763ca9500d2e7c85e63af16ff5b24240
Nakiti <nikhilakiti91@gmail.com>
2024-11-21 20:28:43 -0800

    added options

--- backend/index.js
+++ backend/index.js
@@ -27,6 +27,14 @@ app.use(express.json())
 app.use(cors(corsOptions))
 app.use(cookieParser())
 
+app.options('*', (req, res) => {
+   res.header('Access-Control-Allow-Origin', req.headers.origin);
+   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+   res.header('Access-Control-Allow-Credentials', 'true');
+   res.sendStatus(200);
+});
+
 app.use("/api/organization", organizationRoutes)
 app.use("/api/campaign", campaignRoutes)
 app.use("/api/donationPage", donationPageRoutes)
