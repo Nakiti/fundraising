8f3e55b0e971403b06f08a70b717c0e37f88bc35
Nakiti <nikhilakiti91@gmail.com>
2024-12-18 11:06:13 -0500

    fixed edit

--- frontend/src/app/org/[organizationId]/campaign/edit/[id]/page.js
+++ frontend/src/app/org/[organizationId]/campaign/edit/[id]/page.js
@@ -1,67 +1,13 @@
 "use client"
-import { useContext, useEffect, useState } from "react"
-import { CampaignContext } from "@/app/context/campaignContext"
-import { AuthContext } from "@/app/context/authContext"
-import { createCampaignDesignation, deleteCampaignDesignation, updateCampaign, updatePreview } from "@/app/services/campaignService"
-import { useRouter } from "next/navigation"
 import NewLayout from "../../new/layout"
 
 const EditCampaign = ({params}) => {
-   const [active, setActive] = useState("preview")
    const campaignId = params.id
-   const organizationId = params.organizationId
-   const {previewInputs, settingsInputs, selectedDesignations} = useContext(CampaignContext)
-   const {currentUser} = useContext(AuthContext)
-   const router = useRouter()
  
-   const handleActiveChange = (tab) => {
-      setActive(tab)
-   }
-
-   const handlePublish = async() => {
-      try {
-         await updateCampaign(campaignId, settingsInputs, "active", currentUser)
-         await updatePreview(campaignId, previewInputs)
-         await deleteCampaignDesignation(campaignId)
-         await createCampaignDesignation(campaignId, selectedDesignations)
-
-         router.push(`/org/${organizationId}/dashboard/campaigns`)
-      } catch (err) {
-         console.log(err)
-      }
-   }
-
-   const handleSave = async() => {
-      try {
-         await updateCampaign(campaignId, settingsInputs, 'inactive', currentUser);
-         await updatePreview(campaignId, previewInputs);
-         await deleteCampaignDesignation(campaignId)
-         await createCampaignDesignation(campaignId, selectedDesignations)
-
-         router.push(`/org/${organizationId}/dashboard/campaigns`)
-
-      } catch (err) {
-         console.log(err)
-      }
-   }
-
-   const handleDeactivate = async () => {
-      try {
-         await updateCampaign(campaignId, settingsInputs, 'inactive', currentUser);
-      } catch (err) {
-         console.log(err)
-      }
-   }
-
    return (
-      <NewLayout>
+      <div>
          {campaignId}
-      </NewLayout>
-      // <div className="w-full bg-gray-50">
-      //    <Navbar active={active} title={"Edit Campaign"} handleActiveChange={handleActiveChange} handlePublish={handlePublish} handleSave={handleSave} handleDeactivate={handleDeactivate}/>
-
-      //    {active == "settings" ?  <Settings /> : <Preview />}
-      // </div>
+      </div>
    )
 }
 
